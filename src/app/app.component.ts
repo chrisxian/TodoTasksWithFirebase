ebde665478587387a7cfb442e52110aa2dfc5322
chrisxian <leixian929@gmail.com>
2019-01-22 14:45:21 +0800

    two small refactorings: 1. introduce type "Task". 2. extract firestore collection endpoint to config file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { config } from './app.config';
+import { Task } from './app.model';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +12,11 @@ import { Observable } from 'rxjs';
 export class AppComponent implements OnInit{
   
   title = 'app';
-  tasks: Observable<any[]>;
+  tasks: Observable<Task[]>;
 
   constructor(private db: AngularFirestore){}
 
   ngOnInit(): void {
-    this.tasks = this.db.collection('tasks').valueChanges();
+    this.tasks = this.db.collection(config.collection_endpoint).valueChanges() as Observable<Task[]>;   
   }
-
 }
