5c90d550da8b924574f72cf753b6423afc0594ec
chrisxian <leixian929@gmail.com>
2019-01-26 14:51:18 +0800

    minor change: add comments

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -14,6 +14,9 @@ export class AppService {
   tasks: AngularFirestoreCollection<Task>;
   private taskDoc : AngularFirestoreDocument<Task>;
 
+  //Injectables are just normal classes (normal objects) and as such, they have no special lifecycle.
+  //When an object of your class is created, the class’s constructor is called, so that’s what your “OnInit” would be.
+  //https://stackoverflow.com/questions/36188966/life-cycle-methods-for-services-in-angular2
   constructor(private db: AngularFirestore) {
       this.tasks = db.collection<Task>(config.collection_endpoint);
    }
